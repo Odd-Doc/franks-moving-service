4c23993c4247effcfecbdbfa8106e73d9284af33
Odd-Doc <gsphillipsjr@gmail.com>
2024-08-29 13:20:00 -0500

    button start

--- client/src/components/QuoteReq/GetAQuote.jsx
+++ client/src/components/QuoteReq/GetAQuote.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from "react";
 import { ModalWrapper, StyledForm } from "./style";
-import {
-  Input,
-  Modal,
-  ModalActions,
-  Button,
-  ModalHeader,
-  ModalContent,
-} from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
 
+////////////////////////////////////////////////////////////////////////
+// Modal Button
+////////////////////////////////////////////////////////////////////////
+const ModalButton = () => {
+  return <button>Get a Quote</button>;
+};
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 const FormInput = ({ ...props }) => {
   return (
     <Input
@@ -50,87 +52,7 @@ export const QuoteModal = () => {
   };
   return (
     <ModalWrapper>
-      <Modal
-        onClose={() => setFirstOpen(false)}
-        onOpen={() => setFirstOpen(true)}
-        open={firstOpen}
-        trigger={
-          <Button
-            size="huge"
-            style={{ backgroundColor: "#e76f51ff", color: "white" }}
-            onClick={() => setFirstOpen(true)}
-          >
-            Get a Quote
-          </Button>
-        }
-        size="tiny"
-      >
-        <StyledForm>
-          <div>
-            <h3 className="quote-title">We just need a few details!</h3>
-          </div>
-
-          <FormInput
-            onChange={handleNameChange}
-            type="text"
-            id="fullName"
-            name="lname"
-            value={fullName}
-            placeholder="Full name"
-            className="last-name--quote"
-            icon="map pin"
-          />
-
-          <FormInput
-            onChange={handleMovingFrom}
-            type="text"
-            id="movingFrom"
-            name="moving-from"
-            value={movingFrom}
-            placeholder="Moving from"
-            className="moving-from--quote"
-            icon="map pin"
-          />
-
-          <FormInput
-            onChange={handleMovingTo}
-            type="text"
-            id="movingTo"
-            name="moving-to"
-            value={movingTo}
-            placeholder="Moving to"
-            className="moving-to--quote"
-            icon="map pin"
-          />
-
-          <div className="button-container">
-            <Button content={"Submit"} color="google plus" onClick={onSubmit} />
-          </div>
-        </StyledForm>
-        {/* nested confirmation modal */}
-        <Modal
-          onClose={() => setSecondOpen(false)}
-          open={secondOpen}
-          size="mini"
-          dimmer="blurring"
-        >
-          <ModalHeader>Sent!</ModalHeader>
-          <ModalContent>
-            <p>I'll get right back to you!</p>
-          </ModalContent>
-          <ModalActions>
-            <Button
-              icon="check"
-              content="All Done"
-              color="green"
-              onClick={() => {
-                setSecondOpen(false);
-                setFirstOpen(false);
-              }}
-            />
-          </ModalActions>
-        </Modal>
-      </Modal>
+      <ModalButton></ModalButton>
     </ModalWrapper>
   );
 };
