623e9dd73e6cd998aa836ab1e04d364ca6667e2f
Odd-Doc <gsphillipsjr@gmail.com>
2024-08-29 12:03:01 -0500

    About me complete

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,21 +1,32 @@
 import { useInView } from "react-intersection-observer";
-import { Container } from "./style";
+import { AboutImage, Container, Content, Title, AboutMe } from "./style";
 
 export const About = () => {
   return (
     <Container className="container">
-      Hi, I'm Frank, the proud owner of our local moving company. With over 10
-      years of experience in the moving industry, I've built this business on
-      the principles of hard work, integrity, and exceptional customer service.
-      My team and I are dedicated to making your move as smooth and stress-free
-      as possible. We handle every aspect of your relocation with care, from
-      packing to transportation, so you can focus on settling into your new
-      space. No job is too big or small for us—we treat every move as if it were
-      our own. I'm committed to providing transparent pricing with no hidden
-      fees and ensuring that every customer feels confident and satisfied with
-      our services. When you choose us, you're not just getting movers; you're
-      getting a team that cares about making your transition a positive
-      experience.
+      <Content>
+        {" "}
+        <div>
+          <AboutImage />
+        </div>
+        <AboutMe>
+          <Title className="ubuntu-bold">Who am I?</Title>
+          <p>
+            Hi, I'm Frank, the proud owner of our local moving company. With
+            over 10 years of experience in the moving industry, I've built this
+            business on the principles of hard work, integrity, and exceptional
+            customer service. My team and I are dedicated to making your move as
+            smooth and stress-free as possible. We handle every aspect of your
+            relocation with care, from packing to transportation, so you can
+            focus on settling into your new space. No job is too big or small
+            for us—we treat every move as if it were our own. I'm committed to
+            providing transparent pricing with no hidden fees and ensuring that
+            every customer feels confident and satisfied with our services. When
+            you choose us, you're not just getting movers; you're getting a team
+            that cares about making your transition a positive experience.
+          </p>
+        </AboutMe>
+      </Content>
     </Container>
   );
 };
--- client/src/components/about/style.jsx
+++ client/src/components/about/style.jsx
@@ -1,8 +1,52 @@
-import par2 from "../../assets/images/parallax-bg-2.png";
 import styled from "styled-components";
+import aboutMe from "../../assets/images/Logistics.png";
+import { mediaQuery } from "../media-query";
+export const Title = styled.h2`
+  margin: 0;
+  padding-bottom: 1rem;
+  font-size: 2em;
+  ${mediaQuery.md`
+    text-align: center;
 
+    
+  `}
+`;
 export const Container = styled.div`
-  height: 100vh;
-  background-image: url(${par2});
+  color: black;
+  /* background-color: ${(props) => props.theme.colors.persiangreen}; */
+  background-color: white;
+  padding: 2rem;
+`;
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  ${mediaQuery.md`
+    flex-direction:column;
+    align-items: center;
+    
+  `}
+`;
+export const AboutImage = styled.div`
+  height: 400px;
+  width: 400px;
   background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url(${aboutMe});
+  /* ${mediaQuery.md`
+    
+    
+  `} */
+`;
+export const AboutMe = styled.div`
+  padding: 2rem;
+  p {
+    font-size: 1.5em;
+    margin-top: 1rem;
+  }
+  ${mediaQuery.md`
+    padding-left:1rem;
+    padding-right:1rem;
+    text-align:center;
+    
+  `}
 `;
