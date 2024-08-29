89d39c20844abf9c2503ef36b1127e48fc1d39fd
Odd-Doc <gsphillipsjr@gmail.com>
2024-08-29 08:05:46 -0500

    image resize to 1200x800 on Service detailes section

--- client/src/components/service-details/services.details.jsx
+++ client/src/components/service-details/services.details.jsx
@@ -1,4 +1,11 @@
-import { Container, Service, Title } from "./style";
+import {
+  Container,
+  Service,
+  ServiceDescription,
+  ServicesImage,
+  Title,
+  Content,
+} from "./style";
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -7,49 +14,78 @@ export const ServiceDetails = () => {
   return (
     <Container className="container">
       <Service>
-        <Title className="service-title ubuntu-bold">
-          Residential Moving
-          {/* <hr /> */}
-        </Title>
-        Moving to a new home can be both exciting and stressful. Our Residential
-        Moving Services are designed to make your move as smooth and hassle-free
-        as possible. We handle every aspect of your relocation with care and
-        professionalism, ensuring that your belongings arrive safely at your new
-        home.
+        <ServiceDescription>
+          <div className="service-img-container">
+            <ServicesImage id="service-img1" />
+          </div>
+          <Content>
+            <Title className="service-title ubuntu-bold">
+              Residential Homes
+            </Title>
+            Moving to a new home can be both exciting and stressful. Our
+            Residential Moving Services are designed to make your move as smooth
+            and hassle-free as possible. We handle every aspect of your
+            relocation with care and professionalism, ensuring that your
+            belongings arrive safely at your new home.
+          </Content>
+        </ServiceDescription>
       </Service>
       <Service>
-        <Title className="service-title ubuntu-bold">
-          Office Moving
-          {/* <hr /> */}
-        </Title>
-        Our professional office moving service is designed to ensure a seamless
-        and stress-free relocation for businesses of all sizes. We understand
-        that minimizing downtime and disruption is critical, so we offer
-        comprehensive solutions tailored to meet your specific needs.
+        <ServiceDescription>
+          <Content>
+            <Title className="service-title ubuntu-bold">Apartments</Title>
+            Dont worry, there are{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "#ebebeb",
+                textDecoration: "underline",
+              }}
+            >
+              no stairway charges!
+            </span>{" "}
+            Our apartment moving service is designed to make your relocation
+            experience seamless and stress-free. Whether you're moving within
+            the same building, across town, or to a new city, our professional
+            team is here to handle every aspect of your move with care and
+            efficiency.
+          </Content>{" "}
+          <div className="service-img-container">
+            <ServicesImage id="service-img2" />
+          </div>
+        </ServiceDescription>
+      </Service>
+      {/* <Service>
+        <Title className="service-title ubuntu-bold">Office Moving</Title>
+        <ServiceDescription>
+          Our professional office moving service is designed to ensure a
+          seamless and stress-free relocation for businesses of all sizes. We
+          understand that minimizing downtime and disruption is critical, so we
+          offer comprehensive solutions tailored to meet your specific needs.
+        </ServiceDescription>
       </Service>
       <Service>
-        <Title className="service-title ubuntu-bold">
-          Packing Services
-          {/* <hr /> */}
-        </Title>
-        packing service ensures your belongings are carefully and efficiently
-        packed for a stress-free move. Whether you need full-service packing,
-        where we handle everything from your fragile items to everyday
-        essentials, or just assistance with specific items, our experienced team
-        is equipped to protect your possessions.
+        <Title className="service-title ubuntu-bold">Packing Services</Title>
+        <ServiceDescription>
+          packing service ensures your belongings are carefully and efficiently
+          packed for a stress-free move. Whether you need full-service packing,
+          where we handle everything from your fragile items to everyday
+          essentials, or just assistance with specific items, our experienced
+          team is equipped to protect your possessions.
+        </ServiceDescription>
       </Service>
-
       <Service>
         <Title className="service-title ubuntu-bold">
           Loading and Unloading
-          {/* <hr /> */}
         </Title>
-        Our Loading and Unloading Service ensures a smooth and efficient moving
-        experience by handling the heavy lifting for you. Whether you're
-        relocating locally or long-distance, our skilled team is trained to
-        safely load and unload your belongings, minimizing the risk of damage
-        during transit.
-      </Service>
+        <ServiceDescription>
+          Our Loading and Unloading Service ensures a smooth and efficient
+          moving experience by handling the heavy lifting for you. Whether
+          you're relocating locally or long-distance, our skilled team is
+          trained to safely load and unload your belongings, minimizing the risk
+          of damage during transit.
+        </ServiceDescription>
+      </Service> */}
     </Container>
   );
 };
--- client/src/components/service-details/style.jsx
+++ client/src/components/service-details/style.jsx
@@ -1,24 +1,65 @@
 import par1 from "../../assets/images/parallax-bg-1.png";
 import styled from "styled-components";
+import movingCouch from "../../assets/images/moving-couch.jpg";
+import apartmentMove from "../../assets/images/moving-apartment.jpg";
 
+export const Content = styled.div`
+  background-color: ${(props) => props.theme.colors.persiangreen};
+  padding-top: 80px;
+  padding-bottom: 80px;
+  box-shadow: 5px 7px 20px #2e2e2e;
+`;
+export const ServicesImage = styled.div`
+  height: 400px;
+  width: 400px;
+  border-radius: 100%;
+  background-size: cover;
+  box-shadow: 5px 0px 20px #2e2e2e;
+`;
 export const Service = styled.div`
   font-size: 1.2rem;
-  display: flex;
-  text-align: center;
   line-height: 1.5em;
   padding: 2rem;
-  display: block;
 `;
 
 export const Title = styled.h3`
   font-size: 2rem;
 `;
 export const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.offwhite};
-  flex-direction: column;
-  ${Title} {
+  background-color: ${(props) => props.theme.colors.saffron};
+  color: white;
+  box- ${Title} {
     font-size: 2em;
   }
-  hr {
+  .service-img-container {
+    z-index: 1;
   }
+  #service-img1 {
+    background-image: url(${movingCouch});
+  }
+  #service-img2 {
+    background-image: url(${apartmentMove});
+    background-position: 83%;
+  }
+  :nth-child(odd).service-img-container {
+    margin-right: -100px;
+  }
+  :nth-child(even).service-img-container {
+    margin-left: -100px;
+  }
+  :nth-child(odd) ${Content} {
+    padding-left: 10rem;
+    padding-right: 1rem;
+    border-radius: 0 8px 8px 0px;
+  }
+  :nth-child(even) ${Content} {
+    padding-right: 8rem;
+    padding-left: 4rem;
+    border-radius: 8px 0px 0px 8px;
+  }
+`;
+
+export const ServiceDescription = styled.div`
+  display: flex;
+  align-items: center;
 `;
